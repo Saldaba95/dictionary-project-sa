3a7687d398f0a92d6d80cd6966186c8de0843a4e
Saldaba95 <aldaba.sarei1995@gmail.com>
2024-07-14 22:04:17 -0500

    created new component
    added another component
    changed the back to all black/dark grayish
    added div with classname container

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import cata from "./cata.jpg";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <div className="row">
-          <div className="col-4">
-            <img src={cata} className="catalina" alt="logo" />
+      <div className="container">
+        <header className="App-header">
+          <div className="row">
+            <div className="col-4">
+              <img src={cata} className="catalina" alt="logo" />
+            </div>
+            <div className="col-8">
+              {" "}
+              <h1>Welcome to Catalina's Dictionary</h1>
+              <Dictionary />
+            </div>
           </div>
-          <div className="col-8">
-            {" "}
-            <h1>Hello Everyone</h1>
-          </div>
-        </div>
-      </header>
+        </header>
+        <main></main>
+        <footer>
+          Coded by Sarei Aldaba. It's open source on GitHub and Netlify
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword} definition`);
+  }
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      {" "}
+      <form onSubmit={search}>
+        {" "}
+        <input type="search" onChange={handleKeywordChange} />{" "}
+        <button
+          className="
+        btn btn-primary"
+        >
+          Search
+        </button>
+      </form>{" "}
+    </div>
+  );
+}
