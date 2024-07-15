f9522bfb97457b67f2d0c8fa303539eef34dd580
Saldaba95 <aldaba.sarei1995@gmail.com>
2024-07-14 21:04:19 -0500

    added image

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import cata from "./cata.jpg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="row">
+          <div className="col-4">
+            <img src={cata} className="catalina" alt="logo" />
+          </div>
+          <div className="col-8">
+            {" "}
+            <h1>Hello Everyone</h1>
+          </div>
+        </div>
       </header>
     </div>
   );
