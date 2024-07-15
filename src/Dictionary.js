0286a036409199a81fef7842b81fecb9b854ab0d
Saldaba95 <aldaba.sarei1995@gmail.com>
2024-07-14 22:37:11 -0500

    api/url
    added axios
    added function for api and console.log word entered

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,13 +1,24 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response);
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword} definition`);
+
+    let apiKey = "d44bo97ccfe0cbf0e78f5498fb0fa93t";
+
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
+
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
